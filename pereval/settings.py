9de0a09c778ffb92303625cc4efdd44cb60d8557
xRA1Nx <stwski@gmail.com>
2022-06-23 18:44:55 +0300

    some security fixes

--- pereval/settings.py
+++ pereval/settings.py
@@ -26,11 +26,11 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'perevaldb',
-        'USER': 'postgres',
+        'NAME': os.getenv("DB_NAME"),
+        'USER':  os.getenv("DB_USER"),
         'PASSWORD': os.getenv("DB_PASS"),
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'HOST':  os.getenv("DB_HOST"),
+        'PORT':  os.getenv("DB_PORT"),
     },
 }
 
